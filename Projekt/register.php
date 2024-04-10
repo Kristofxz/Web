28c318aa6cfaa5ce59bd774212aa87dc3b9e7f26
Kristofxz <160526942+Kristofxz@users.noreply.github.com>
2024-04-10 17:59:56 +0200

    Add files via upload

---
+++ Projekt/register.php
@@ -0,0 +1,8 @@
+<?php
+
+$servername = "localhost";
+$username = "felhasznalonev";
+$password = "jelszo";
+$dbname = "adatbazis_nev";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
